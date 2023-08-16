ad5dc73c584b23b14abb80ecfb691e4f963ecd93
mindkeeper <fcb.nyak@gmail.com>
2023-08-16 14:10:18 +0700

    creating normalized email and admin

--- app/core/models.py
+++ app/core/models.py
@@ -7,16 +7,24 @@ from django.contrib.auth.models import (
 
 class CustomerManager(BaseUserManager):
 
-    def create_customer(self, email, phone_number, name, password,
+    def create_customer(self, email, name, password,
                         **extra_fields):
+        if not email:
+            raise ValueError("Email is Required")
         customer = self.model(
-            email=email,
-            phone_number=phone_number,
+            email=self.normalize_email(email),
             name=name,
             **extra_fields)
         customer.set_password(password)
         customer.save(using=self._db)
         return customer
+
+    def create_admin(self, email, password, **extra_fields):
+        admin = self.create_customer(email, "admin", password)
+        admin.is_superuser = True
+        admin.set_password(password)
+        admin.save(using=self._db)
+        return admin
 
 
 class Customer(AbstractBaseUser, PermissionsMixin):
@@ -34,7 +42,8 @@ class Customer(AbstractBaseUser, PermissionsMixin):
         max_length=10,
         choices=GENDER_CHOICES,
         null=True)
-    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True, blank=True,
+                                    null=True)
     image = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,14 +6,39 @@ class ModelTest(TestCase):
     def test_create_customer_with_email_successfull(self):
         email = 'test@example.com'
         password = 'test123'
-        phone_number = '081234567890'
         name = 'john doe'
         customer = get_user_model().objects.create_customer(
             email=email,
             password=password,
-            phone_number=phone_number,
             name=name,
         )
 
         self.assertEqual(customer.email, email)
         self.assertTrue(customer.check_password(password))
+
+    def test_new_cutomer_email_nornalized(self):
+        sample_test = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_test:
+            user = get_user_model().objects.create_customer(
+                email=email,
+                name='test',
+                password='test123'
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raise_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_customer('', 'test', 'test123')
+
+    def test_create_admin(self):
+        admin = get_user_model().objects.create_admin(
+            'test@example.com',
+            'test123'
+            )
+        self.assertTrue(admin.is_superuser)
